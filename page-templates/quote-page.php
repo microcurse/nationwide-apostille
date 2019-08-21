a236262c5d54c1bbfc86aabf6fc970c40461ff86
Marc <marc.maninang@gmail.com>
2019-08-20 22:39:48 -0700

    Not finished adding front end ACF form
    Still working on displaying the form on the front end using genesis_entry_content hook

--- page-templates/quote-page.php
+++ page-templates/quote-page.php
@@ -21,6 +21,19 @@ add_action( 'genesis_after_header', 'genesis_do_post_title', 6 );
 // Close Hero Banner
 add_action( 'genesis_after_header', 'hero_banner_close', 15 );
 
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+add_action( 'genesis_entry_content', 'custom_loop' );
+
+function custom_loop() { ?>
+  <article itemtype="http://schema.org/CreativeWork" itemscope="itemscope" class="post-<?php print $pageid; ?> page type-page status-publish entry">
+  <div class="entry-content" itemprop="text">
+    <?php if( have_posts() ) : while( have_posts() ) : the_post();
+    echo get_field('country');
+    acf_form(); 
+    endwhile; endif;
+    ?> 
+  </div></article>
+<?php }
 
 // Runs the Genesis loop.
 genesis();
