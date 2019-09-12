f1fac25cd6a3414e2b8da12c151df5af005a33d8
Marc <marc.maninang@gmail.com>
2019-09-11 18:41:39 -0700

    removed unused loop functions
    didn't work..

--- page-templates/apostille-page.php
+++ page-templates/apostille-page.php
@@ -21,17 +21,5 @@ add_action( 'genesis_after_header', 'genesis_do_post_title', 6 );
 // Close Hero Banner
 add_action( 'genesis_after_header', 'hero_banner_close', 15 );
 
-add_action( 'genesis_after_content', 'show_state_requirements_posts', 20);
-
-add_action('genesis_loop', 'custom_loop');
-
-function custom_loop() {
-
-  global $paged;
-  $args = array('post_type' => 'state_requirements' );
-
-  genesis_custom_loop( $args );
-}
-
 // Runs the Genesis loop.
 genesis();
